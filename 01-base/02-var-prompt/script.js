94d2e490ac37cb5c1a2348894f17e80b95069b9c
Kim Couchard <kcouchard@gmail.com>
2020-06-24 14:19:20 +0200

    asynchronous

--- 01-base/02-var-prompt/script.js
+++ 01-base/02-var-prompt/script.js
@@ -8,7 +8,6 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
-    var getName;
     var getName = prompt("What's your name?");
-    alert("Hello "+getName+" !");
+    alert("Hello " + getName + " !");
 
